9a7862876af6b5fa4b0dd07c58c1604ea995d355
Kipiatkova <kipiatkova1998@mail.ru>
2022-01-16 10:52:49 +0300

    New commit

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,20 @@ public class Main {
     public static void main(String[] args) {
         int start = 100;
         int plus = 1100;
-        int bonus = plus / 100;
-        int all = start + plus + bonus;
-        int finish = start + plus;
         if (plus > 1000) {
-            System.out.println(all);
-        }
-        else {
-            if (plus < 1000) {
+            int bonus = (plus / 100);
+            int all = start + plus + bonus;
+            {
+                System.out.println(all);
+            }
+        } else {
+            int finish = start + plus;
+            {
                 System.out.println(finish);
             }
+
         }
     }
 }
+
+
